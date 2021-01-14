fd33abe127e42401252a6a4a89f8da246343cf34
Taylor Thompson <tthompson0217@myduneland.org>
2021-01-14 10:47:01 -0600

    completed shapes

--- Picture.java
+++ Picture.java
@@ -27,6 +27,22 @@ public class Picture {
         a.makeVisible();
         Rect d = new Rect(100, 300, 400, 450, Canvas.getColor("red"));
         d.makeVisible();
+        Circle i = new Circle(690, 490, 300, Canvas.getColor("white"));
+        i.makeVisible();
+        Circle f = new Circle(520, 890, 720, Canvas.getColor("cyan"));
+        f.makeVisible();
+        Arc g = new Arc(300, 200, 200, 80, 320, Canvas.getColor("white"));
+        g.makeVisible();
+        Arc h = new Arc(20, 500, 700, 35, 90, Canvas.getColor("brown"));
+        h.makeVisible();
+        Triangle j = new Triangle(100, 200, 500, 100, Canvas.getColor("green"));
+        j.makeVisible();
+        Triangle k = new Triangle(250, 300, 300, 600, Canvas.getColor("yellow"));
+        k.makeVisible();
+        Rect l = new Rect(150, 100, 310, 120, Canvas.getColor("green"));
+        l.makeVisible();
+        Rect m = new Rect(300, 250, 600, 200, Canvas.getColor("cyan"));
+        m.makeVisible();
 
         // Get the filename to save to from the command line arguments, defaulting to
         // MyPicture.png if no argument is given
